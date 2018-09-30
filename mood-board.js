4f4b0cc6420f6895bc484a6239136304766bc54c
chriswillphoto <christophernekich@gmail.com>
2018-10-01 01:19:33 +1000

    changed name, fixed child node count bug

--- mood-board.js
+++ mood-board.js
@@ -24,14 +24,13 @@ var masonryCols = function(settings) {
   gridSettings.breakpoints[3] = gridSettings.breakpoints[3] || 992;
   gridSettings.breakpoints[4] = gridSettings.breakpoints[4] || 1200;
 
-  console.log(gridSettings)
+  
 
   var grid = document.querySelector("." + containerClass);
-  childList = grid.children;
+  var childList = [];
 
-  for (var i = 0; i < childList.length; i++) {
-    var tile = childList[i];
-    tile.classList.add("Mood-tile");
+  for (var node = 0; node < grid.children.length; node++) {
+    childList.push(grid.children[node])
   }
 
   if (window.innerWidth < gridSettings.breakpoints[1]) {
@@ -46,7 +45,32 @@ var masonryCols = function(settings) {
     columnCount = 5;
   }
 
-  console.log(columnCount, window.innerWidth);
+  for (var i=1; i<=columnCount; i++){
+    var col = document.createElement('div');
+    col.classList.add('column-'+i)
+    col.classList.add('Mood-column')
+    grid.appendChild(col)
+  }
+
+  for (var i = 0; i < childList.length; i++) {
+    var tile = childList[i];
+    tile.classList.add("Mood-tile");
+    tile.setAttribute("data-index", i + 1);
+
+    var cols = document.querySelectorAll('.Mood-column')
+
+    for (var j=columnCount; j > 0; j--){
+      var tileIndex = tile.getAttribute("data-index")
+      if( parseInt(tileIndex) % j == 0 ){
+        console.log(tileIndex, j)
+        break
+        // toAppend = tile.parentNode.removeChild(tile);
+        // cols[j].appendChild(toAppend)
+      }
+    }
+  }
+
+  // console.log(columnCount, window.innerWidth);
 };
 
 masonryCols({ container: "whatever" });
