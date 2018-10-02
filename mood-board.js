8455f94e1d65266b06698e3a191dadc22e5b4084
chriswillphoto <christophernekich@gmail.com>
2018-10-02 19:04:47 +1000

    rewrote functions using while loops - resize function now works

--- mood-board.js
+++ mood-board.js
@@ -24,7 +24,7 @@ var establishSettings = function(settings){
   return returnSettings
 }
 
-var columnCounter = function(gridSettings){
+var columnCountSet = function(gridSettings){
   var columnCount;
 
   if (window.innerWidth < gridSettings.breakpoints[1]) {
@@ -43,67 +43,160 @@ var columnCounter = function(gridSettings){
 }
 
 var addColumns = function(grid, currentCols, columnCount){
-  for (var i = 1 + currentCols; i <= columnCount; i++) {
+
+  columnList = document.querySelectorAll('.Mood-column');
+  // for ( var i=0; i < columnList.length; i++ ){
+  //   columnList[i].parentNode.removeChild(columnList[i])
+  // }
+
+  // removes all columns
+
+  while( grid.lastChild ){
+    grid.removeChild(grid.lastChild)
+  }
+
+  var columnIndex = currentCols + 1;
+
+  while ( grid.children.length < columnCount ){
     var col = document.createElement("div");
-    col.classList.add("column-" + i);
+    col.classList.add("column-" + columnIndex);
     col.classList.add("Mood-column");
     col.style.width = 100 / columnCount + "%";
     col.style.textAlign = "center";
     col.style.boxSizing = "border-box";
     grid.appendChild(col);
+    
+    columnIndex += 1;
   }
+
+  // for (var i = 1 + currentCols; i <= columnCount; i++) {
+  //   var col = document.createElement("div");
+  //   col.classList.add("column-" + i);
+  //   col.classList.add("Mood-column");
+  //   col.style.width = 100 / columnCount + "%";
+  //   col.style.textAlign = "center";
+  //   col.style.boxSizing = "border-box";
+  //   grid.appendChild(col);
+  // }
 }
+
+var addContent = function(tileList, columnCount){
+
+  var cols = document.querySelectorAll(".Mood-column");
+  // console.log(cols)
+  if ( cols.length == columnCount ){
+    for (var i = 0; i < tileList.length; i++) {
+      var tile = tileList[i];
+      tile.classList.add("Mood-tile");
+      tile.style.marginLeft = "auto";
+      tile.style.marginRight = "auto";
+      tile.style.width = "100%";
+      tile.setAttribute("data-index", i + 1);
+      for (var j = columnCount; j > 0; j--) {
+        var tileIndex = parseInt(tile.getAttribute("data-index"));
+        while (tileIndex > columnCount) {
+          tileIndex -= columnCount;
+        }
+  
+        if (tileIndex % j == 0) {
+          // console.log(tileIndex, j)
+          // console.log(tile)
+          cols[j - 1].appendChild(tile);
+          break;
+        }
+      }
+    }
+  }
+  
+}
+
+// var debug = function(){
+//   var test = columnCounter(establishSettings())
+//   console.log(test)
+// }
+
+
 
 var masonryCols = function(settings) {
   
 
   var gridSettings = establishSettings(settings);
-  var columnCount = columnCounter(gridSettings);
-
-
-  var grid = document.querySelector("." + gridSettings.container);
-  var tileList = [];
-
-  for (var node = 0; node < grid.children.length; node++) {
-    tileList.push(grid.children[node]);
-  }
-
-
-
+  var columnCount = columnCountSet(gridSettings);
   var columnList = document.querySelectorAll('.Mood-column');
   var currentCols = columnList.length;
 
+
+  var grid = document.querySelector("." + gridSettings.container);
+  var tileList = [].slice.call(grid.children);
+  var itemCount = parseInt(grid.children.length)
+
+
+  while (grid.lastChild){
+    grid.removeChild(grid.lastChild)
+  }
+
+  // console.log(tileList)
+  // for (var node = 0; node < itemCount; node++) {
+  //   // console.log(grid.children)
+  //   grid.children[node].classList.add("Mood-tile");
+  //   // tileList.push(grid.children[node]);
+    
+  //   grid.children[node].classList.add("delete");
+  //   // grid.children[node].parentElement.removeChild(grid.children[node])
+
+  // }
+
   if( currentCols < columnCount ){
     addColumns(grid, currentCols, columnCount);
+    addContent(tileList, columnCount)
   }
 
+  window.addEventListener('resize', function(){
+    columnCount = columnCountSet(gridSettings);
+    columnList = document.querySelectorAll('.Mood-column');
+    currentCols = columnList.length;
 
-
-
-  for (var i = 0; i < tileList.length; i++) {
-    var tile = tileList[i];
-    tile.classList.add("Mood-tile");
-    tile.style.marginLeft = "auto";
-    tile.style.marginRight = "auto";
-    tile.style.width = "100%";
-    tile.setAttribute("data-index", i + 1);
-
-    var cols = document.querySelectorAll(".Mood-column");
-
-    for (var j = columnCount; j > 0; j--) {
-      var tileIndex = parseInt(tile.getAttribute("data-index"));
-      while (tileIndex > columnCount) {
-        tileIndex -= columnCount;
-      }
-
-      if (tileIndex % j == 0) {
-        // console.log(tileIndex, j)
-        toAppend = tile.parentNode.removeChild(tile);
-        cols[j - 1].appendChild(toAppend);
-        break;
-      }
+    if( currentCols < columnCount ){
+      removeColumns()
+      addColumns(grid, currentCols, columnCount)
+      addContent(tileList, columnCount)
+    }else if ( currentCols > columnCount){
+      removeColumns();
+      addColumns(grid, currentCols, columnCount);
+      addContent(tileList, columnCount)
     }
-  }
+  }) // eventlistener
+
+
+
+
+
+
+
+  // for (var i = 0; i < tileList.length; i++) {
+  //   var tile = tileList[i];
+  //   // tile.classList.add("Mood-tile");
+  //   tile.style.marginLeft = "auto";
+  //   tile.style.marginRight = "auto";
+  //   tile.style.width = "100%";
+  //   tile.setAttribute("data-index", i + 1);
+
+  //   var cols = document.querySelectorAll(".Mood-column");
+
+  //   for (var j = columnCount; j > 0; j--) {
+  //     var tileIndex = parseInt(tile.getAttribute("data-index"));
+  //     while (tileIndex > columnCount) {
+  //       tileIndex -= columnCount;
+  //     }
+
+  //     if (tileIndex % j == 0) {
+  //       // console.log(tileIndex, j)
+  //       toAppend = tile.parentNode.removeChild(tile);
+  //       cols[j - 1].appendChild(toAppend);
+  //       break;
+  //     }
+  //   }
+  // }
 
   // console.log(columnCount, window.innerWidth);
 };
